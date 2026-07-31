41882fb41d557c73be810ce1b2a50610c851b375
Xnine Volt <davidx9volt@gmail.com>
2026-07-31 14:56:01 +0100

    Implement responsive navigation menu toggle

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navMenu = document.querySelector("nav ul");
+const menuIcon = document.querySelector(".menu-btn i");
+
+menuBtn.addEventListener("click", () => {
+
+    navMenu.classList.toggle("show");
+
+    if (navMenu.classList.contains("show")) {
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-xmark");
+    } else {
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+    }
+
+});
